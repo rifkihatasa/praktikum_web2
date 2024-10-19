5716f1f58324efa9e9225312891276a8987bf605
rifki hatasa <rifkihatasa12@gmail.com>
2024-10-19 15:21:00 +0800

    file login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap Example</title>
+        {{-- include bootstrap--}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body class="p-3 m-0 border-0 bd-example">
+            <div class="row">
+                <h1>Muhammad Rifki Hatasa</h1>
+                <div class="card">
+                <div class="card-body">
+                    <div class="mb-3">
+                        <label class="form-label">Email address</label>
+                        <input type="email" class="form-control" autocomplete="new-password" placeholder="youremail@gmail.com">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Password</label>
+                        <input type="password" class="form-control" autocomplete="new-password" placeholder="password">
+            </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
